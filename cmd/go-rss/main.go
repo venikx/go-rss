1be1caa37c4bd52e01755d66c6ea8a238437d121
Kevin Rangel <code@venikx.com>
2024-03-21 18:08:05 +0100

    chore(envrc): can read env variables

--- cmd/go-rss/main.go
+++ cmd/go-rss/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello Nix!")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Printf("Running server on http://localhost:%s", port)
 }
